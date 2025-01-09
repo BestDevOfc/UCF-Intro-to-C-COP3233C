c868a88d066e23bc77eb3eb66ec477c35b4775f8
alimuhammadsecured <164618346+BestDevOfc@users.noreply.github.com>
2025-01-09 18:54:27 -0500

    Update WhyPointerTypes.c

--- WhyPointerTypes.c
+++ WhyPointerTypes.c
@@ -11,6 +11,8 @@ int main(){
     // will be the same size, but the reason it needs to know is because data is stored differently!
     // for example, integer can be stored wherever (in stack since it's statically allocated by compiler)
     // buttt, the test is also stored in the stack but stored sequentially because that's how the data type is stored.
+    // because remember, the compiler just converts all of your code into assembly, so it needs to know how to 
+    // design that assembly to store that specific data type.
     int* a = NULL;
     printf("Size of a int pointer --> %lu\n", sizeof(a));
 
